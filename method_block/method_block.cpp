1ae66e35a10bf0d759a3ea1a375843debd4a7133
Alex <tseplyaeff.alex@yandex.ru>
2020-01-01 13:50:49 +0900

    .

--- method_block/method_block.cpp
+++ method_block/method_block.cpp
@@ -25,23 +25,19 @@ int main()
 	vector_float Q1; // Обратная матрица к Q
 	vector_float M1; // Обратная матрица к M
 
-	Q1 = reverse(Q); // шаг первый
-	M1 = reverse(M); // шаг второй
+	Q1 = reverse(Q); // Q^-1
+	M1 = reverse(M); // M^-1
 
-	// шаг третий
 	// получение матрицы X по формуле X = (M - NQ^-1*P)^-1
 	vector_float X = formula_XV(M, N, Q1, P);
 
-	// шаг четвертый
 	// получение матрицы V по формуле V = (Q - PM^-1*N)^-1
 	vector_float V = formula_XV(Q, P, M1, N);
 
-	// шаг пятый
 	// получение матрицы U по формуле U = -VPM^-1
 	vector_float U = formula_UY(V, P, M1);
 
-	// шаг шестой
-	// получение матрицы U по формуле U = -VPM^-1
+	// получение матрицы Y по формуле Y = -XNQ^-1
 	vector_float Y = formula_UY(X, N, Q1);
 
 	printVector(X);
